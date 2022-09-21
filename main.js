10c790cbd89bfe1dae14579ca8477a4ab44cb4be
Orel-Viktor <OrelViktor93@gmail.com>
2022-09-21 15:29:30 +0300

    TwoParagRedy

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-import './style.css'
+// Создать функцию для расчета произведения двух чисел, 
+// вызываемого следующим образом: name(5)(2). Функция
+//  должна возвращать результат (в середине функции не 
+// должно быть консоль лога!)
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function getMultiNumber(number1, number2) {
+  let resMulti = number1 * number2
+  return resMulti
+}
+const multiNumber = getMultiNumber(5, 2)
+console.log(multiNumber)
+
+
+
+// Создать функцию, вычисляющую 
+// среднее арифметическое числовых элементов массива любой длины.
+
+let numArray = [1, 2, 3]
+function getMidArray() {
+  result = numArray.reduce(
+    (accum, item) =>
+      accum = accum + item
+  )
+  return result / numArray.length
+}
+const midResult = getMidArray(numArray)
+console.log(midResult)
