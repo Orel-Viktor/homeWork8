6a055de0d5f73f97759014294fa28d8575663666
Orel-Viktor <OrelViktor93@gmail.com>
2022-09-22 13:55:57 +0300

    homeWork8-redy

--- main.js
+++ main.js
@@ -3,11 +3,12 @@
 //  должна возвращать результат (в середине функции не 
 // должно быть консоль лога!)
 
-function getMultiNumber(number1, number2) {
-  let resMulti = number1 * number2
-  return resMulti
+function getMultiNumber(number1) {
+  return (number2) => {
+    return number1 * number2
+  }
 }
-const multiNumber = getMultiNumber(5, 2)
+const multiNumber = getMultiNumber(5)(2)
 console.log(multiNumber)
 
 
@@ -15,7 +16,7 @@ console.log(multiNumber)
 // Создать функцию, вычисляющую 
 // среднее арифметическое числовых элементов массива любой длины.
 
-let numArray = [1, 2, 3]
+let numArray = [1, 2, 3, 4, 5, 6, 78]
 function getMidArray() {
   result = numArray.reduce(
     (accum, item) =>
@@ -23,5 +24,23 @@ function getMidArray() {
   )
   return result / numArray.length
 }
-const midResult = getMidArray(numArray)
+const midResult = getMidArray()
 console.log(midResult)
+
+
+// Создать функцию, которая удаляет из строки все символы, переданные вторым аргументом. 
+// 'func("hello world", ['l', 'd'])' вернет нам "heo wor"
+
+
+function replaceChars(str = "", chars) {
+  let newArr = []
+  for (let char of str.split('')) {
+    if (!chars.includes(char)) {
+      newArr.push(char)
+    }
+  }
+  return newArr.join('')
+}
+
+const getReplaceChars = replaceChars("Hello world", ['l', 'd'])
+console.log(getReplaceChars)
